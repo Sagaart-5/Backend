b0002aca761bcc900d84289b1476f3537e44b0bf
mxnoob <noobmx@gmail.com>
2024-06-26 12:36:32 +0300

    [update] art filter

--- backend/api/v1/arts/filters.py
+++ backend/api/v1/arts/filters.py
@@ -1,11 +1,20 @@
 from django.db.models import Q
-from django_filters.rest_framework import CharFilter, FilterSet
+from django_filters.rest_framework import (
+    CharFilter,
+    FilterSet,
+    MultipleChoiceFilter,
+)
 
 from arts.models import Art
 
 
 class ArtsFilterSet(FilterSet):
     price = CharFilter(method="get_price")
+    category = CharFilter(method="add_filter", field_name="category__name")
+    orientation = CharFilter(method="add_filter", field_name="orientation__type")
+    color = CharFilter(method="add_filter", field_name="color__type")
+    style = CharFilter(method="add_filter", field_name="style__name")
+    size = CharFilter(method="add_filter", field_name="size__name")
 
     class Meta:
         model = Art
@@ -18,3 +27,11 @@ class ArtsFilterSet(FilterSet):
                 min_price, max_price = price_range.strip().split("-")
                 q_price |= Q(price__gte=min_price, price__lte=max_price)
         return queryset.filter(q_price)
+
+    @staticmethod
+    def add_filter(queryset, name, value):
+        q = Q()
+        if value:
+            for params in value.split(","):
+                q |= Q(**{name: params})
+        return queryset.filter(q)
